e47632ce139586806bd05178090cf7b71a6ba86d
OLUWASEUN <ogunjimi@systemspecs.com.ng>
2017-07-05 17:36:38 +0100

    Merge branch 'master' of https://bitbucket.org/verumtmbp/verum-web-ui
                                   # Conflicts:
                                   #	src/app/core/core.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  data: Object[];
+  ajax: DataTables.FunctionAjax;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.data = [
+      {field: 'vin', header: 'Vin', name: 'Tiger Nixon', job: "System Architect"},
+      {field: 'year', header: 'Year', name: 'Garrett Winters', job: "Accountant"},
+      {field: 'brand', header: 'Brand', name: 'Angelica', job: "Chief Executive Officer (CEO)"},
+      {field: 'color', header: 'Color', name: '$170,750', job: "System Architect"}
+    ];
+    let cc = [
+      {field: 'vin', header: 'Vin', name: 'Tiger Nixon', job: "System Architect"},
+      {field: 'year', header: 'Year', name: 'Garrett Winters', job: "Accountant"},
+      {field: 'brand', header: 'Brand', name: 'Angelica', job: "Chief Executive Officer (CEO)"},
+      {field: 'color', header: 'Color', name: '$170,750', job: "System Architect"},
+      {field: 'color', header: 'Color', name: '$170,750', job: "System Architect"}
+    ];
+    let x = 1;
+    this.ajax = (data: any, callback, settings) => {
+      //let this_ = this;
+      setTimeout(() => {
+        x++;
+        //this.ajaxDataListener.next(v);
+        callback({
+          "draw": data.draw,
+          "recordsTotal": 57,
+          "recordsFiltered": 57,
+          data: x % 2 < 2 ? this.data : cc
+        });
+      }, 200);
+    };
+  }
+
+  _onClick($event: any, $implicitData?: any): void {
+    console.log($event);
+    console.log($implicitData);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from "./app.component";
+import {DatatablesModule} from "./datatables/datatables.module";
+import * as $ from "jquery";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, DatatablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+window['jQuery'] = window['$'] = $;
+
+const WIDGET_PLUGINS = [
+  require("datatables.net"),
+  require("datatables.net-bs"),
+  require("datatables.net-select"),
+];
