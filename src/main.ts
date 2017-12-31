53f2639d72d6ea70204c0a811aef834e131883b4
OLUWASEUN <ogunjimi@systemspecs.com.ng>
2017-12-31 14:02:02 +0100

    ignore intellij project file

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {environment} from './environments/environment';
-import {DemoModule} from "./demo/demo.module";
+import {DemoModule} from './demo/demo.module';
 
 if (environment.production) {
   enableProdMode();
